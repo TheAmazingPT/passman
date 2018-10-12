19a077aa72962390046d1acd37d15a6b91a82871
The Amazing PT <theamazingpt@protonmail.com>
2018-10-13 00:59:51 +0200

    add checks for not adding empty strings to pass

--- src/add-password.sh
+++ src/add-password.sh
@@ -2,11 +2,23 @@ function add_password {
   local prompt="Enter Accountname:"
   local account_name=$(echo | dmenu -i -p "$prompt")
 
+  if [[ -z $account_name ]]; then
+    exit
+  fi
+
   local prompt="Enter Password:"
   local password_1=$(echo | dmenu -i -p "$prompt")
 
+  if [[ -z $password_1 ]]; then
+    exit
+  fi
+
   local prompt="Enter Password again:"
   local password_2=$(echo | dmenu -i -p "$prompt")
+
+  if [[ -z $password_2 ]]; then
+    exit
+  fi
 
   if [[ $password_1 == $password_2 ]]; then
     echo "$password_1" | pass insert --echo "$account_name" 1>/dev/null
