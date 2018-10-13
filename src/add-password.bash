7f417f8342f6f77efb00714d50a21b0b3acee63a
The Amazing PT <theamazingpt@protonmail.com>
2018-10-13 14:59:07 +0200

    add pasting password from clipboard in add password menu

--- src/add-password.bash
+++ src/add-password.bash
@@ -1,5 +1,6 @@
 function add_password {
-  local prompt account_name menu generated_password password_1 password_2
+  local prompt account_name menu generated_password password_1 password_2 \
+    pasted_password confirmed_pasted_password
 
   prompt="Enter Accountname: (service/username)"
   account_name=$(echo | dmenu -i -p "$prompt")
@@ -9,18 +10,38 @@ function add_password {
   fi
 
   prompt="Enter Password:"
-  menu="\"Generate Password\""
-  password_1=$(echo "$menu"| xargs -n 1 | dmenu -i -p "$prompt")
+
+  menu=(
+    "\"Paste from Clipboard\""
+    "\"Generate Password\""
+  )
+
+  password_1=$(echo "${menu[@]}"| xargs -n 1 | dmenu -i -p "$prompt")
 
   if [[ -z "$password_1" ]]; then
     exit
-  elif [[ $password_1 == "Generate Password" ]]; then
-    generated_password=$(generate_password 32 1)
-    password_1=$generated_password
-    password_2=$generated_password
   fi
 
-  if [[ -z "$password_2" ]]; then
+  case $password_1 in
+    "Paste from Clipboard")
+      pasted_password=$(xclip -o -selection clipboard)
+      ;;
+
+    "Generate Password")
+      generated_password=$(generate_password 32 1)
+      password_1=$generated_password
+      password_2=$generated_password
+      ;;
+  esac
+
+  if [[ -n "$pasted_password" ]]; then
+    prompt="Enter Password (pasted):"
+    confirmed_pasted_password=$(echo "$pasted_password" | dmenu -i -p "$prompt")
+    password_1=$confirmed_pasted_password
+    password_2=$confirmed_pasted_password
+  fi
+
+  if [[ -z "$pasted_password" && -z "$password_2" ]]; then
     prompt="Enter Password again:"
     password_2=$(echo | dmenu -i -p "$prompt")
   fi
