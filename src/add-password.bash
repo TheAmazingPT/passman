76857001e5181006af8801a8b83206b2cda8f839
The Amazing PT <theamazingpt@protonmail.com>
2018-10-13 12:37:13 +0200

    add password generator when adding a new password

--- src/add-password.bash
+++ src/add-password.bash
@@ -1,5 +1,5 @@
 function add_password {
-  local prompt account_name password_1 password_2
+  local prompt account_name menu generated_password password_1 password_2
 
   prompt="Enter Accountname: (service/username)"
   account_name=$(echo | dmenu -i -p "$prompt")
@@ -9,14 +9,21 @@ function add_password {
   fi
 
   prompt="Enter Password:"
-  password_1=$(echo | dmenu -i -p "$prompt")
+  menu="\"Generate Password\""
+  password_1=$(echo "$menu"| xargs -n 1 | dmenu -i -p "$prompt")
 
   if [[ -z "$password_1" ]]; then
     exit
+  elif [[ $password_1 == "Generate Password" ]]; then
+    generated_password=$(generate_password 32 1)
+    password_1=$generated_password
+    password_2=$generated_password
   fi
 
-  prompt="Enter Password again:"
-  password_2=$(echo | dmenu -i -p "$prompt")
+  if [[ -z "$password_2" ]]; then
+    prompt="Enter Password again:"
+    password_2=$(echo | dmenu -i -p "$prompt")
+  fi
 
   if [[ -z "$password_2" ]]; then
     exit
--- src/generate-password.bash
+++ src/generate-password.bash
@@ -1,12 +1,18 @@
 function generate_password {
-  local default_length length generated_password trimmed_password
+  local default_length length use_echo generated_password trimmed_password
 
   default_length=32
   length=${1:-$default_length}
+  use_echo=${2:-0}
 
   generated_password="$(head -c 512 /dev/urandom | sha512sum | base64)"
   trimmed_password="$(echo "$generated_password" | head -c "$length")"
 
-  clip "$trimmed_password"
-  notification "New password generated!" 1>/dev/null
+  if [[ use_echo -eq 1 ]]; then
+    notification "New password generated!" 1>/dev/null
+    echo "$trimmed_password"
+  else
+    notification "New password generated!" 1>/dev/null
+    clip "$trimmed_password"
+  fi
 }
